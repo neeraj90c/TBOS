de8eb9f59a7f781aea70d89310321670fa901699
SumitGoreSystel <139208351+SumitGoreSystel@users.noreply.github.com>
2024-05-23 14:27:26 +0530

    customer form fix

--- AngularClient/src/app/TBOS/Master/Customer/customer-master/customer-master.component.ts
+++ AngularClient/src/app/TBOS/Master/Customer/customer-master/customer-master.component.ts
@@ -4,11 +4,18 @@ import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { PaginatedDTO } from 'src/app/TBOS/common.interface';
 import { AgentMasterService } from '../../Agent/agent-master.service';
 import { AgentMaster } from '../../Agent/agent.interface';
-import { CustomerMasterDTOPaginated } from '../customer.interface';
+import { CreateCustomer, CustomerMasterDTO, CustomerMasterDTOPaginated, UpdateCustomer } from '../customer.interface';
 import { CustomerService } from '../customer.service';
 import { TransportMasterService } from '../../Transport/transport-master.service';
 import { TransportMaster } from '../../Transport/transport.interface';
 import { AddressDetailService } from 'src/app/TBOS/UC/Address/address-detail.service';
+import { AuthenticationService } from 'src/app/Common/Authentication/authentication.service';
+import { User } from 'src/app/Common/Authentication/auth.interface';
+import { updateContact, updateCustomer } from 'src/app/GlobalVariables';
+import { ContactDetailService } from 'src/app/TBOS/UC/Contact/contact-detail.service';
+import { forkJoin, switchMap } from 'rxjs';
+import { CreateAddress } from 'src/app/TBOS/UC/Address/address.interface';
+import { CreateContact } from 'src/app/TBOS/UC/Contact/contact.interface';
 
 @Component({
   selector: 'app-customer-master',
@@ -23,13 +30,16 @@ export class CustomerMasterComponent implements OnInit {
     private agentMasterService: AgentMasterService,
     private transportMasterService: TransportMasterService,
     private addressService: AddressDetailService,
+    private contactService: ContactDetailService,
+    private authService: AuthenticationService,
     private modalService: NgbModal
   ) { }
 
-  customerList: CustomerMasterDTOPaginated[] = []
-  agentList: AgentMaster[] = []
+  User: User = this.authService.User();
+  customerList: CustomerMasterDTOPaginated[] = [];
+  agentList: AgentMaster[] = [];
   transportList: TransportMaster[] = [];
-  customerModal!: NgbModalRef
+  customerModal!: NgbModalRef;
 
   customerForm = new FormGroup({
     customerId: new FormControl(0),
@@ -53,15 +63,23 @@ export class CustomerMasterComponent implements OnInit {
     panNo: new FormControl(''),
     priority: new FormControl(''),
     customerRemarks: new FormControl(''),
-    cGST: new FormControl(''),
-    sGST: new FormControl(''),
-    iGST: new FormControl(''),
-    uTGST: new FormControl(''),
+    cGST: new FormControl('N'),
+    sGST: new FormControl('N'),
+    iGST: new FormControl('N'),
+    uTGST: new FormControl('N'),
     gSTIN_No: new FormControl(''),
     gSTReverseCharge: new FormControl(''),
-    exportGST: new FormControl(''),
-    actionUser: new FormControl(0)
+    exportGST: new FormControl('N'),
+    actionUser: new FormControl(this.User.userId)
   });
+
+  get taxFormCTRL(): FormControl {
+    return this.customerForm.controls.taxForm
+  }
+
+  get customerNameCTRL(): FormControl {
+    return this.customerForm.controls.customerName
+  }
 
   addressForm = new FormGroup({
     detailId: new FormControl(0),
@@ -75,7 +93,8 @@ export class CustomerMasterComponent implements OnInit {
     state: new FormControl(''),
     country: new FormControl(''),
     pincode: new FormControl(''),
-    status: new FormControl(0)
+    status: new FormControl(2),
+    actionUser: new FormControl(this.User.userId)
   })
 
   conactForm = new FormGroup({
@@ -87,7 +106,7 @@ export class CustomerMasterComponent implements OnInit {
     mobileNumber: new FormControl(''),
     emailId: new FormControl(''),
     contactStatus: new FormControl(0),
-    actionUser: new FormControl(''),
+    actionUser: new FormControl(this.User.userId)
   })
 
   mainForm = new FormGroup({
@@ -121,22 +140,121 @@ export class CustomerMasterComponent implements OnInit {
   openCustomerModal() {
     this.customerModal = this.modalService.open(this.customerModalContent, { size: 'xl' })
   }
-  formSubmit() {
-    console.log(this.mainForm.value);
+  createCustomer() {
+    this.mainForm.markAllAsTouched()
+
+    let newCustomer = {}
+
+    if (this.mainForm.touched && this.mainForm.valid) {
+
+      let contactForm = { ...this.customerForm.value }
+      let createCustomer: CreateCustomer = {
+        customerName: contactForm.customerName as string,
+        transportId: contactForm.transportId as number,
+        paymentTerm: contactForm.paymentTerm as number,
+        branchId: contactForm.branchId as number,
+        customerBranch: contactForm.customerBranch as string,
+        status: contactForm.status as number,
+        taxForm: contactForm.taxForm as string,
+        tin_No: contactForm.tin_No as string,
+        agentCommission: contactForm.agentCommission as number,
+        paymentDay: contactForm.paymentDay as string,
+        creditDaysLock: contactForm.creditDaysLock as number,
+        cstTinNo: contactForm.cstTinNo as string,
+        customerType: contactForm.customerType as string,
+        defaultPrice: contactForm.defaultPrice as string,
+        defaultInvoice: contactForm.defaultInvoice as string,
+        creditAmountLock: contactForm.creditAmountLock as number,
+        panNo: contactForm.panNo as string,
+        priority: contactForm.priority as string,
+        customerRemarks: contactForm.customerRemarks as string,
+        cGST: contactForm.cGST as string,
+        sGST: contactForm.sGST as string,
+        iGST: contactForm.iGST as string,
+        uTGST: contactForm.uTGST as string,
+        gSTIN_No: contactForm.gSTIN_No as string,
+        gSTReverseCharge: contactForm.gSTReverseCharge as string,
+        exportGST: contactForm.exportGST as string,
+        actionUser: this.User.userId.toString()
+      }
+      this.customerService.CreateCustomer(createCustomer).pipe(switchMap(customerResponse => {
+        customerResponse
+        let addressForm = { ...this.addressForm.value }
+        let createAddress: CreateAddress = {
+          masterCode: customerResponse.customerCode,
+          addressType: addressForm.addressType as string,
+          add_line1: addressForm.add_line1 as string,
+          add_line2: addressForm.add_line2 as string,
+          add_line3: addressForm.add_line3 as string,
+          add_line4: addressForm.add_line4 as string,
+          city: addressForm.city as string,
+          state: addressForm.state as string,
+          country: addressForm.country as string,
+          pincode: addressForm.pincode as string,
+          status: addressForm.status as number,
+          actionUser: this.User.userId.toString()
+        }
+        let contactForm = { ...this.conactForm.value }
+        let createContact: CreateContact = {
+          masterCode: customerResponse.customerCode,
+          personName: contactForm.personName as string,
+          designation: contactForm.designation as string,
+          phoneNumber: contactForm.phoneNumber as string,
+          mobileNumber: contactForm.mobileNumber as string,
+          emailId: contactForm.emailId as string,
+          contactStatus: contactForm.contactStatus as number,
+          actionUser: this.User.userId.toString()
+        }
+        const createAddress$ = this.addressService.CreateAddress(createAddress)
+        const createContact$ = this.contactService.CreateContact(createContact)
+        return forkJoin([createAddress$, createContact$]);
+      })).subscribe(
+        ([addressResponse, contactResponse]) => {
+          console.log('Address created:', addressResponse);
+          console.log('Contact created:', contactResponse);
+        },
+        error => {
+          console.error('Error occurred:', error);
+        }
+      )
+
+    }
 
   }
 
 
-  onGstChange(selectedGst: string) {
-    this.customerForm.patchValue({
-      cGST: selectedGst === 'cGST' ? 'Y' : 'N',
-      sGST: selectedGst === 'sGST' ? 'Y' : 'N',
-      iGST: selectedGst === 'iGST' ? 'Y' : 'N',
-      uTGST: selectedGst === 'uTGST' ? 'Y' : 'N',
-      exportGST: selectedGst === 'exportGST' ? 'Y' : 'N'
-    });
-    console.log(this.customerForm.value);
-    
+  onGstChange(gstType: string) {
+    switch (gstType) {
+      case 'cGST':
+        this.customerForm.patchValue({ cGST: this.customerForm.value.cGST === 'Y' ? 'N' : 'Y' });
+        this.customerForm.patchValue({ sGST: this.customerForm.value.cGST });
+        this.customerForm.patchValue({ iGST: 'N' });
+        this.customerForm.patchValue({ uTGST: 'N' });
+        break;
+      case 'sGST':
+        this.customerForm.patchValue({ sGST: this.customerForm.value.sGST === 'Y' ? 'N' : 'Y' });
+        this.customerForm.patchValue({ cGST: this.customerForm.value.sGST });
+        this.customerForm.patchValue({ iGST: 'N' });
+        this.customerForm.patchValue({ uTGST: 'N' });
+        break;
+      case 'iGST':
+        this.customerForm.patchValue({ cGST: 'N' });
+        this.customerForm.patchValue({ sGST: 'N' });
+        this.customerForm.patchValue({ iGST: 'Y' });
+        this.customerForm.patchValue({ uTGST: 'N' });
+        break;
+      case 'uTGST':
+        this.customerForm.patchValue({ cGST: 'N' });
+        this.customerForm.patchValue({ sGST: 'N' });
+        this.customerForm.patchValue({ iGST: 'N' });
+        this.customerForm.patchValue({ uTGST: 'Y' });
+        break;
+      case 'exportGST':
+        this.customerForm.patchValue({ exportGST: this.customerForm.value.exportGST === 'Y' ? 'N' : 'Y' });
+        break;
+      default:
+        break;
+    }
   }
 
 }
--- AngularClient/src/app/TBOS/Master/Customer/customer.service.ts
+++ AngularClient/src/app/TBOS/Master/Customer/customer.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CustomerList, CustomerListPaginated } from './customer.interface';
-import { getAllCustomers, getAllCustomersPaginated } from '../../../GlobalVariables';
+import { CreateCustomer, CustomerList, CustomerListPaginated, CustomerMasterDTO } from './customer.interface';
+import { createCustomer, getAllCustomers, getAllCustomersPaginated } from '../../../GlobalVariables';
 import { PaginatedDTO } from '../../common.interface';
 
 @Injectable({
@@ -18,6 +18,10 @@ export class CustomerService {
 
   ReadAllCustomerPaginated(data: PaginatedDTO): Observable<CustomerListPaginated> {
     return this.http.post<CustomerListPaginated>(getAllCustomersPaginated, data)
+  }
+
+  CreateCustomer(data: CreateCustomer): Observable<CustomerMasterDTO> {
+    return this.http.post<CustomerMasterDTO>(createCustomer, data)
   }
 
 }
